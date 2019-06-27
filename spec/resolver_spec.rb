74e1bd3781282fffac95b689c23e6c3d5d3c6ea3
Aaron Christiansen <aaronc20000@gmail.com>
2019-06-27 17:59:17 +0100

    Add Resolver tests

---
+++ spec/resolver_spec.rb
@@ -0,0 +1,128 @@
+require 'yard'
+
+describe Sord::Resolver do
+  before do
+    YARD::Registry.clear
+    Sord::Logging.silent = true
+    Sord::Resolver.clear
+  end
+
+  def at(name)
+    YARD::Registry.at(name)
+  end
+
+  it 'returns a sensible list of built-in classes' do
+    expect(subject.builtin_classes).to include 'String', 'Numeric', 'Integer', 'Float', 'IO'
+  end
+
+  it 'resolves built-in classes without ambiguity' do
+    YARD.parse_string(<<-EOF)
+      class A
+      end
+    EOF
+
+    subject.prepare
+    expect(subject.resolvable?('String', at('A'))).to be true
+  end
+
+  it 'does not resolve built-in classes with ambiguity' do
+    YARD.parse_string(<<-EOF)
+      module A
+        class String
+        end
+
+        class B
+        end
+      end
+    EOF
+
+    subject.prepare
+    expect(subject.resolvable?('String', at('A::B'))).to be false
+    expect(subject.path_for('String')).to be nil
+  end
+
+  it 'resolves parent modules' do
+    YARD.parse_string(<<-EOF)
+      module A
+        module B
+          module C
+            class D
+            end
+          end
+        end
+      end
+    EOF
+
+    subject.prepare
+    expect(subject.resolvable?('B', at('A::B::C::D'))).to be true
+  end
+
+  it 'resolves siblings of parent modules' do
+    YARD.parse_string(<<-EOF)
+      module A
+        module B
+          module C
+            class D
+            end
+          end
+        end
+
+        class E
+        end
+      end
+    EOF
+
+    subject.prepare
+    expect(subject.resolvable?('E', at('A::B::C::D'))).to be true
+  end
+
+  it 'can infer unresolvable module structures without ambiguity' do
+    YARD.parse_string(<<-EOF)
+      module A
+        module B
+          module C
+            class D
+            end
+          end
+        end
+
+        module E
+          class F
+          end
+        end
+      end
+    EOF
+
+    subject.prepare
+    p Sord::Resolver.class_variable_get :@@names_to_paths
+    expect(subject.resolvable?('F', at('A::B::C::D'))).to be false
+    expect(subject.path_for('F')).to eq 'A::E::F'
+  end
+
+  it 'does not resolve ambiguity' do
+    YARD.parse_string(<<-EOF)
+      module A
+        module B
+          module C
+            class D
+            end
+          end
+        end
+
+        module E
+          class F
+          end
+        end
+
+        module G
+          class F
+          end
+        end
+      end
+    EOF
+
+    subject.prepare
+    expect(subject.resolvable?('F', at('A::B::C::D'))).to be false
+    expect(subject.path_for('F')).to be nil
+  end
+end
